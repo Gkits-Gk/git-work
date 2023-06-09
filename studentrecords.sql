d3bd1cfe8f41ad5f0c36f02e412af6d9be8f1060
thv2246 <134273532+thv2246@users.noreply.github.com>
2023-06-09 11:25:31 +0530

    studentrecords.sql

--- studentrecords.sql
+++ studentrecords.sql
@@ -1,12 +1,16 @@
 create database studentdata;
 CREATE TABLE `practice1` (
   `id` int(11) DEFAULT NULL,
-  `subjectname` varchar(55) DEFAULT NULL,
-  `subjectmarks` int(4) DEFAULT NULL
+  studentname	varchar(50)		Yes	NULL				
+	3	physics	int(3)			Yes	NULL		
+	4	chemistry	int(3)			Yes	NULL			
+	5	maths	int(3)			Yes	NULL			
+	6	total	int(3)	
 );
 INSERT INTO `practice1` (`id`, `subjectname`, `subjectmarks`) VALUES
-(1, 'physics', 89),
-(2, 'chemistry', 92),
-(3, 'English', 98),
-(4, 'social', 96);
+(1,Harsha,58,57,60,175),
+(2,sirisha,69,78,87,234),
+(3,ravi,99,86,71,236),
+(4,rahul,100,98,80,278),
+(5,kishore,65,70,85,220);
 select * from practice1;
