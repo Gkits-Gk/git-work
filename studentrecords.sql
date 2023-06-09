a65cd487b561962fe6142a15ff55b4e296936594
Gkits-Gk <thegkraju@gmail.com>
2023-06-09 12:14:43 +0530

    Tested Harsha

--- studentrecords.sql
+++ studentrecords.sql
@@ -1,12 +1,9 @@
-create database studentdata;
+
+//Okay Harsha tested your sql FILE.. Keep Learing..
+
+use database testmysql1;
 CREATE TABLE `practice1` (
   `id` int(11) DEFAULT NULL,
   `subjectname` varchar(55) DEFAULT NULL,
   `subjectmarks` int(4) DEFAULT NULL
 );
-INSERT INTO `practice1` (`id`, `subjectname`, `subjectmarks`) VALUES
-(1, 'physics', 89),
-(2, 'chemistry', 92),
-(3, 'English', 98),
-(4, 'social', 96);
-select * from practice1;
